5455d4207a60102415ed4580d5eae28525a875c1
zulfikar4568 <isnaen70@gmail.com>
2020-10-28 01:51:58 +0700

    Menambahkan POST DATA

--- controller.js
+++ controller.js
@@ -31,3 +31,19 @@ exports.tampilberdasarkanid = function (req, res){
             }
         });
 };
+
+//Menambahkan data mahasiswa
+exports.tambahMahasiswa  = function (req, res){
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query("INSERT INTO mahasiswa (nim,nama,jurusan) VALUES(?,?,?)", [nim, nama, jurusan],
+        function (error, rows, fields){
+            if(error){
+                console.log(error);
+            }else{
+                response.ok("Berhasil Menambahkan Data!", res)
+            }
+        });
+};
