97015925523e37bf9f21adb2e1948fcff279c235
zulfikar4568 <isnaen70@gmail.com>
2020-10-28 01:34:01 +0700

    GET DATA

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi Rest API ku berjalan ok", res);
 };
+
+//menampilkan data semua mahasiswa
+exports.tampilsemua = function (req, res){
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
