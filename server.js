1753aec7eb04f5dd11cff1c6f329f18172bd4b37
zulfikar4568 <isnaen70@gmail.com>
2020-10-28 01:05:17 +0700

    setting routes di server

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 //parse aplikasi json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server started on port`);
