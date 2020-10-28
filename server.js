10892a6a7669d651515f99679720829309def8ba
zulfikar4568 <isnaen70@gmail.com>
2020-10-28 13:47:47 +0700

    Registrasi dengan JWT berhasil

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var morgan = require('morgan');
 const app = express();
 
 //parse aplikasi json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 //panggil routes
 var routes = require('./routes');
 routes(app);
+
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
 
 const port = 3000;
 app.listen(port, () => {
